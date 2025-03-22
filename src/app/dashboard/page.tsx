dc3abc9ff4cfffbd40039448b159f03b58d6225e
LaiAnTan <tanlaian2004@gmail.com>
2025-03-23 02:59:32 +1100

    feat: dashboard ui in progress

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,95 @@
+'use client'
+
+import Header from "@/components/Header";
+import LoanInfoCard from "@/components/LoanInfoCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { motion } from "motion/react";
+import { i } from "motion/react-client";
+
+function LoanInfoCarousel() {
+
+  const loans = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+  return (
+    <Carousel
+    className="flex justify-center w-[95%]"
+    opts={{
+      align: 'center',
+      loop: true
+    }}>
+      <CarouselPrevious />
+        <CarouselContent>
+          {Array.from({ length: 10 }).map((_, index) => (
+              <CarouselItem key={index} className="basis-1/4">
+                <LoanInfoCard />
+              </CarouselItem>
+          ))}
+        </CarouselContent>
+      <CarouselNext />
+    </Carousel>
+  )
+
+}
+
 export default function Dashboard() {
-    return (
-        <div>Woahh look at this cool dashhboard woahhhhhhh pizza</div>
-    )
+
+  const username = "User"
+
+  const balance = 10000
+  const currency = "USDC"
+
+
+  return (
+    <div className="flex flex-col h-screen w-screen bg-[#EFF8FC] items-center">
+      <Header />
+      <div className="p-12 w-full">
+        <div className="flex flex-col space-y-8 w-full">
+          <div className="px-10 font-semibold text-black text-5xl">Welcome back, {username}!</div>
+          <div className="flex flex-col space-y-5 px-10">
+            <div className="font-semibold text-black text-4xl">Total Balance</div>
+            <div className="flex flex-row justify-start items-end space-x-5">
+              <div className="font-bold text-black text-6xl">{balance.toLocaleString()} USDC</div>
+                <motion.button
+                  className="flex justify-center w-12 h-12 bg-[#4b48fd] rounded-full text-white font-semibold text-4xl text-center cursor-pointer select-none"
+                  whileHover={{
+                    scale: 1.1
+                  }}
+                  whileTap={{
+                    scale: 0.9
+                  }}
+                >
+                  +
+                </motion.button>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between px-10">
+            <div className="text-black text-3xl font-bold">Recent Loans</div>
+            <motion.button
+            className="w-48 h-12 bg-[#4b48fd] rounded-lg text-white font-semibold text-lg cursor-pointer select-none"
+            whileHover={{
+              scale: 1.1
+            }}
+            whileTap={{
+              scale: 0.9
+            }}
+            >
+              New Loan
+            </motion.button>
+          </div>
+          <div className="flex justify-center">
+            <LoanInfoCarousel />
+          </div>
+          <div className="flex flex-row justify-between px-10">
+            <div className="text-black text-3xl font-bold">History</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ src/components/LoanInfoCard/index.tsx
@@ -0,0 +1,61 @@
+import { motion, AnimatePresence } from "motion/react"
+import { useState } from "react"
+
+export default function LoanInfoCard() {
+
+  const loanedAmount = 1000
+  const loanName = "House"
+  const repaymentAmount = 500
+  const dueDate = new Date(2025, 6, 1)
+  
+  const [hovering, setHovering] = useState(false)
+
+  const getDueDate = (date: Date) => {
+
+    const daysUntil = Math.ceil((date.getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
+    const weeksUntil = Math.ceil(daysUntil / 7)
+
+    if (daysUntil > 0 && daysUntil <= 7)
+      return (daysUntil + ' day' + (daysUntil > 1 ? 's' : ''))
+    else if (daysUntil > 7 && daysUntil <= 31)
+      return (weeksUntil + ' week' + (weeksUntil > 1 ? 's' : ''))
+
+    return (date.toLocaleDateString(undefined, { year: 'numeric', month: 'long' }))
+  }
+
+  return (
+    <motion.div
+      className="flex flex-col w-88 h-48 rounded-lg bg-purple-300
+      hover:bg-[#9747FF] hover:text-white transition-all p-5 space-y-1 group relative cursor-pointer select-none"
+      onHoverStart={() => setHovering(true)}
+      onHoverEnd={() => setHovering(false)}
+    >
+      <div className="text-5xl font-bold -ml-1">
+        {loanedAmount} USDC
+      </div>
+      <div className="text-3xl font-semibold ">{loanName}</div>
+      <div className="text-2xl font-semibold">{repaymentAmount}$ due in {getDueDate(dueDate)}</div>
+      <div className="absolute bottom-5 right-5">
+        <AnimatePresence mode="wait">
+          {
+            hovering &&
+            <motion.div
+            className="group flex w-14 h-10 p-4 bg-white text-black rounded-lg items-center
+            justify-center text-xl"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            whileHover={{
+              scale: 1.1
+            }}
+            whileTap={{
+              scale: 0.9
+            }}
+            >
+              Pay
+            </motion.div>
+          }
+        </AnimatePresence>
+      </div>
+    </motion.div>
+  )
+}
