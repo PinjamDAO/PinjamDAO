51b8f71a1dd12e2b39a9e9fbdc0975726f0aa18c
SaveMePlz <chuashixiang@gmail.com>
2025-03-25 16:04:09 +0800

    updated modal

--- src/models/users.ts
+++ src/models/users.ts
@@ -3,21 +3,56 @@ import mongoose from "mongoose"
 export type userType = {
     _id: string, 
     username: string,
+
+    // internal data
     worldId: string,
-    creditScore: Number,
     walletID: string,
     walletAddress: string,
+    
+    // additional datas
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    dateOfBirth: Date,
+    gender: string,
+    ethnicity: string,
+    religion: string,
+    residencyStatus: string,
+    employmentStatus: string,
+    maritialStatus: string,
+    educationLevel: string,
+    phoneNumber: string,
+    address: string,
+    postcode: string,
+    city: string,
+    state: string
 }
 
 const userSchema = new mongoose.Schema({
     username: String,
+
+    // internal data
     worldId: String,
-    creditScore: {
-        type: Number,
-        default: 0
-    },
     walletID: String,
     walletAddress: String,
+
+    // additional datas
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    dateOfBirth: Date,
+    gender: String,
+    ethnicity: String,
+    religion: String,
+    residencyStatus: String,
+    employmentStatus: String,
+    maritialStatus: String,
+    educationLevel: String,
+    phoneNumber: String,
+    address: String,
+    postcode: String,
+    city: String,
+    state: String
 })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
