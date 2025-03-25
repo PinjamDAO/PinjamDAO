97258341c06e1702cf3b1b206f043990ff2c98ca
SaveMePlz <chuashixiang@gmail.com>
2025-03-26 01:17:01 +0800

    feat: added /api/task/[]

---
+++ src/app/api/task/[id]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import Task from "@/models/tasks";
+import { NextApiRequest } from "next";
+import { getCurrentUser } from "@/services/session";
+import { userType } from "@/models/users";
+
+export async function GET(request: NextApiRequest, { params }: { params: { id: string }}) {
+    const user: userType = await getCurrentUser()
+
+    if (user === null) {
+        return NextResponse.json({
+            'msg': 'not log in'
+        }, { status: 401 })
+    }
+
+    const { id } = params
+    const task = await Task.findById(id)
+    if (task.userId !== user.worldId) {
+        return NextResponse.json({
+            'msg': 'no perm'
+        }, { status: 403 })
+    } 
+
+    return NextResponse.json(task)
+}
